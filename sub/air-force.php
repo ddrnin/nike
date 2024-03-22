912bbb51899403cb2c5eee3a480f763c10ed2e75
wjdehdals <wjdehdals145@naver.com>
2024-03-22 11:01:34 +0900

    nike

---
+++ sub/air-force.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>에어포스 1. 나이키 코리아</title>
+</head>
+
+<body>
+
+    <?php
+include_once '../header.html';
+?>
+    <?php
+include_once 'air-force-main.html';
+?>
+    <?php
+include_once '../footer.html';
+?>
+</body>
+
+</html>
